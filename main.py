a401157317003f6ba9c3522804557d45245445e3
myname <780263265@qq.com>
2022-01-12 17:59:02 +0800

    game manu update

--- main.py
+++ main.py
@@ -18,7 +18,8 @@ spi_tx=Pin(3)
 spi0=SPI(0,baudrate=62500000, phase=1, polarity=1, sck=spi_sck, mosi=spi_tx)
 
 
-game=None
+game='Train No.7'
+choose_game=False
 
 display = st77899.ST7789(spi0, disp_width, disp_width,
                           reset=machine.Pin(st7789_res, machine.Pin.OUT),
@@ -34,11 +35,29 @@ display.text(font2, "Welcome!", 60, 110)
 del(display)
 time.sleep(1)
 
-display2.fill_rect(0,0,240,240,color565(30,67,131))
-display2.fill_rect(0,0,240,12,color565(100,23,45))
-time.sleep(1)
-display2.fill_rect(0,0,240,240,0)
-
+display2.fill_rect(0,0,240,240,color565(0,0,0))
+display2.fill_rect(30,70,180,30,color565(45,200,45))
+display2.fill_rect(30,140,180,30,color565(45,200,45))
+display2.rect(30,70,180,30,color565(255,255,255))
+display2.text(font1,"Train No.7",80,80,color565(255,255,255),color565(45,200,45))
+display2.text(font1,"Super Mario",78,150,color565(255,255,255),color565(45,200,45))
 
 my_input = Input()
-game_main.main(display2, my_input)
+while choose_game==False:
+    if my_input.y()==1:
+        display2.rect(30,70,180,30,color565(45,200,45))
+        display2.rect(30,140,180,30,color565(255,255,255))
+        game='Super Mario'
+    elif my_input.y()==-1:
+        display2.rect(30,70,180,30,color565(255,255,255))
+        display2.rect(30,140,180,30,color565(45,200,45))
+        game='Train No.7'
+    if my_input.A():
+        choose_game=True
+        
+display2.fill_rect(0,0,240,240,0)
+if (game=='Train No.7'):
+    #game_main.main(display2, my_input)
+    pass
+elif(game=='Super Mario'):
+    mario_main.main(display2, my_input)
